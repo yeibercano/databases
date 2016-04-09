8ce38b13326ae2be5b9768bd97d50d71374eb662
yeibercano <yeibercano@gmail.com>
2016-04-08 20:04:39 -0700

    making progress

--- server/schema.sql
+++ server/schema.sql
@@ -1,17 +1,17 @@
-CREATE DATABASE chat;
+-- CREATE DATABASE chat;
 
-USE chat;
+-- USE chat;
 
-CREATE TABLE messages (
-  /* Describe your table here.*/
-);
+-- CREATE TABLE messages (id int auto_increment primary key, username varchar(50), room varchar (50), message varchar(1000) mysnot null
+--   /* Describe your table here.*/
+-- );
 
-/* Create other tables and define schemas for them here! */
-
-
+--  Create other tables and define schemas for them here! 
 
 
-/*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
- *  to create the database and the tables.*/
+
+
+-- /*  Execute this file from the command line by typing:
+--  *    mysql -u root < server/schema.sql
+--  *  to create the database and the tables.*/
 
